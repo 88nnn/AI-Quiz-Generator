10bf9810af7ce1569a1ce9a1f6d4965aa38854b8
88nnn <130357067+88nnn@users.noreply.github.com>
2024-05-16 09:05:58 +0900

    Update quiz_creation_page.py

--- quiz_creation_page.py
+++ quiz_creation_page.py
@@ -187,53 +187,53 @@ def quiz_creation_page():
 
             #퀴즈 주제 선택
             language = "언어"
-english = "영어"
-korean = "한국어"
-language_topic = [english, korean]
-
-mathematic = "수리"
-algebra = "대수학"
-geometry = "기하학"
-calculus = "미적분학"
-statistics = "통계학"
-mathematic_topic = [algebra, geometry, calculus, statistics]
-
-social_science = "사회과학"
-psychology = "심리학"
-sociology = "사회학"
-economics = "경제학"
-political_science = "정치학"
-social_science_topic = [psychology, sociology, economics, political_science]
-
-natural_science = "자연과학"
-physics = "물리학"
-chemistry = "화학"
-biology = "생물학"
-astronomy = "천문학"
-natural_science_topic = [physics, chemistry, biology, astronomy]
-
-humanity = "인문학"
-philosophy = "철학"
-history = "역사학"
-literature = "문학"
-art_history = "미술사"
-humanity_topic = [philosophy, history, literature, art_history]
-
-engineering = "공학"
-computer_engineering = "컴퓨터 공학"
-architectural_engineering = "건축공학"
-engineering_topic = [computer_engineering, architectural_engineering]
-
-art = "예술"
-# western_art = "서양화"
-# oriental_art = "동양화"
-film = "영화"
-novel = "소설"
-art_topic = [film,
+            english = "영어"
+            korean = "한국어"
+            language_topic = [english, korean]
+            
+            mathematic = "수리"
+            algebra = "대수학"
+            geometry = "기하학"
+            calculus = "미적분학"
+            statistics = "통계학"
+            mathematic_topic = [algebra, geometry, calculus, statistics]
+            
+            social_science = "사회과학"
+            psychology = "심리학"
+            sociology = "사회학"
+            economics = "경제학"
+            political_science = "정치학"
+            social_science_topic = [psychology, sociology, economics, political_science]
+            
+            natural_science = "자연과학"
+            physics = "물리학"
+            chemistry = "화학"
+            biology = "생물학"
+            astronomy = "천문학"
+            natural_science_topic = [physics, chemistry, biology, astronomy]
+            
+            humanity = "인문학"
+            philosophy = "철학"
+            history = "역사학"
+            literature = "문학"
+            art_history = "미술사"
+            humanity_topic = [philosophy, history, literature, art_history]
+            
+            engineering = "공학"
+            computer_engineering = "컴퓨터 공학"
+            architectural_engineering = "건축공학"
+            engineering_topic = [computer_engineering, architectural_engineering]
+            
+            art = "예술"
+            # western_art = "서양화"
+            # oriental_art = "동양화"
+            film = "영화"
+            novel = "소설"
+            art_topic = [film,
              # western_art,
              # oriental_art,
-             novel]
-            now_topic = [language + "(미지원)", mathematic + "(미지원)",
+            novel]
+            topic = [language + "(미지원)", mathematic + "(미지원)",
              social_science + "(미지원)", natural_science + "(미지원)",
              humanity + "(미지원)", engineering + "(미지원)",
              art + "(영화 분야 지원)"]
