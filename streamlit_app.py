9b5ee6509886f976fa36a8980b55148c1a8a8745
88nnn <130357067+88nnn@users.noreply.github.com>
2024-05-19 10:58:52 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,20 @@ import streamlit as st
 import getpass
 import os
 import io
+import pprint
 from pymongo import MongoClient
 from PyPDF2 import PdfReader
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import MongoDBAtlasVectorSearch
 from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+#from langchain_mongodb import MongoDBAtlasVectorSearch
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 def main():
     st.title("DB 연결 검증")
@@ -21,7 +29,9 @@ def main():
     st.write("MongoDB Atlas Cluster URI has been set.")
 
     # Initialize MongoDB python client
-    client = MongoClient(mongodb_atlas_cluster_uri)
+    client = MongoClient(mongodb_atlas_cluster_uri, server_api=ServerApi('1'))
+
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
     # Define database and collection names
     DB_NAME = "sample_mflix"
@@ -35,21 +45,26 @@ def main():
 
     # Load the PDF
     uploaded_file = st.file_uploader("Upload PDF", type="pdf")
+    text_area_content = ""
+    text_area_content = st.text_area("텍스트를 입력하세요.")
+    text_content = ""
     if uploaded_file is not None:
         data = PdfReader(io.BytesIO(uploaded_file.read()))
-        text_content = ""
         for page in data.pages:
             text_content += page.extract_text()
-
-        if text_content:
             documents = [{"page_content": text_content}]
             st.write(documents)
-        else:
-            st.warning(f"파일에 텍스트가 없습니다.")
+    elif uploaded_file is None:
+        text_content = text_area_content
+    else:
+        st.warning(f"파일에 텍스트가 없습니다.")
 
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
     docs = text_splitter.split_documents(data)
     print(docs[0])
+
+    uploaded_file = st.file_uploader("텍스트, 이미지, 또는 PDF 파일을 업로드하세요.", type=["txt", "jpg", "jpeg", "png", "pdf"])
+    text_area_content = st.text_area("텍스트를 입력하세요.")
 
 # insert the documents in MongoDB Atlas with their embedding
 
