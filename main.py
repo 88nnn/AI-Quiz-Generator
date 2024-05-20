b96dc2736d56e7b00ce89a8398aea41b8fafdf6b
88nnn <130357067+88nnn@users.noreply.github.com>
2024-05-20 12:44:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 import streamlit as st
-import upload_page
 import quiz_creation_page
 import quiz_solve_page
 import quiz_grading_page
 import sign
-import db_connect
-#import embedded_files
-#import topic_creation
 
 def main():
-    # 사이드바 메뉴 설정
-    menu_options = ["로그인", "파일 업로드", "퀴즈 생성", "퀴즈 풀이", "퀴즈 채점"]
-    if 'selected_page' not in st.session_state:
-        st.session_state.selected_page = "퀴즈 생성"
-    selected_page = st.sidebar.radio("메뉴", menu_options, index=menu_options.index(st.session_state.selected_page))
-
+    selected_page = "퀴즈 생성"
+    #showSidebarNavigation = "false"
     # 선택된 페이지 표시
-    if selected_page == "파일 업로드":
-        upload_page.upload_page()
-    elif selected_page == "퀴즈 생성":
+    if selected_page == "퀴즈 생성":
         quiz_creation_page.quiz_creation_page()
     elif selected_page == "퀴즈 풀이":
         quiz_solve_page.quiz_solve_page()
@@ -26,8 +16,6 @@ def main():
         quiz_grading_page.quiz_grading_page()
     elif selected_page == "로그인":
         sign.sign()
-    #elif selected_page == "db기반퀴즈":
-     #   embedded_files.embedded_files()
 
 if __name__ == "__main__":
     main()
