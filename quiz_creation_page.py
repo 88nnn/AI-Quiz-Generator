dd3e1f9f675143a95935fed6fde72a17027ab849
88nnn <130357067+88nnn@users.noreply.github.com>
2024-05-25 12:51:51 +0900

    Update quiz_creation_page.py

--- quiz_creation_page.py
+++ quiz_creation_page.py
@@ -95,6 +95,12 @@ def retrieve_results(user_query):
         search_type = "similarity",
         search_kwargs = {"k": 10, "score_threshold": 0.75}
     )
+    results = vector_search.similarity_search_with_score(
+        user_query = user_query, k = 3
+    )
+
+
+    
     template = """
 Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
