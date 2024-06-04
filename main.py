572a4fc1bf0976b39ac302108e77d3d029346483
88nnn <130357067+88nnn@users.noreply.github.com>
2024-06-04 12:32:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import streamlit as st
-import quiz_creation_page
-import quiz_solve_page
-import quiz_grading_page
-import sign
-#import uplodes
-import topic_creation
-import regist
+import pages.quiz_creation_page
+import pages.quiz_solve_page
+import pages.quiz_grading_page
+import pages.awscog
+from streamlit_option_menu import option_menu
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 def main():
-    selected_page = "퀴즈 생성"
-    #showSidebarNavigation = "false"
-    # 선택된 페이지 표시
+    st.sidebar.title("Navigation")
+    selected_page = st.sidebar.radio("Go to", ["퀴즈 생성", "퀴즈 풀기", "퀴즈 리뷰", 
+                                               "로그인", "회원가입", "퀴즈 저장"])
+
     if selected_page == "퀴즈 생성":
         quiz_creation_page.quiz_creation_page()
-    elif selected_page == "퀴즈 풀이":
+    elif selected_page == "퀴즈 풀기":
         quiz_solve_page.quiz_solve_page()
-    elif selected_page == "퀴즈 채점":
+    elif selected_page == "퀴즈 리뷰":
         quiz_grading_page.quiz_grading_page()
     elif selected_page == "로그인":
-        regist
+        awscog.login()
+    elif selected_page == "회원가입":
+        awscog.sign()  # 회원가입 함수 호출
+    #elif selected_page == "로그인":
+     #   awscog.login()  # 로그인 함수 호출
+
 
 if __name__ == "__main__":
     main()
