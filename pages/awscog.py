f4a4bb0c3ea55a0607816d5e4d43b272938da921
88nnn <130357067+88nnn@users.noreply.github.com>
2024-06-04 20:27:14 +0900

    Update awscog.py

--- pages/awscog.py
+++ pages/awscog.py
@@ -5,50 +5,58 @@ def start():
     placeholder = st.empty()
     if 'user' not in st.session_state:
         st.session_state.user = None  # 초기화
-
-    with placeholder.container():
-        st.title("비회원으로 퀴즈 이용하러 돌아가기")
-        if st.button('퀴즈 생성 바로가기'):
-            st.switch_page("quiz_creation_page")
-
-    # AWS Cognito 설정
-    region_name = 'us-east-1'
-    client_id = '57gm5vjnk9p3ehk9hn5s97ropu'
-    user_pool_id = 'us-east-1_TXA2Lha1Y'
-
-    # Streamlit UI
-    st.header("로그인")
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-
-    if st.button("Login"):
-        cognito_client = boto3.client('cognito-idp', region_name=region_name)
-        try:
-            response = cognito_client.initiate_auth(
-                ClientId=client_id,
-                AuthFlow='USER_PASSWORD_AUTH',
-                AuthParameters={
-                    'USERNAME': username,
-                    'PASSWORD': password
-                }
-            )
-            user = response['AuthenticationResult']
-            st.write(f"Welcome, {username}")
-            st.session_state.user = username  # 유저네임 저장
-        except cognito_client.exceptions.NotAuthorizedException:
-            st.error("인증 실패: 사용자 이름 또는 비밀번호가 올바르지 않습니다.")
-        except Exception as e:
-            st.error(f"오류 발생: {str(e)}")
-
-    if st.button('회원가입'):
-        st.switch_page("sign")
-
+    
     if st.session_state.user:
-        st.write("여기는 로그인한 사용자 전용 서비스입니다.")
-        if st.button("퀴즈 저장"):
-            st.write("저장되셨습니다: 결과")
+        with placeholder.container():
+            st.title("퀴즈 이용하러 돌아가기")
             if st.button('퀴즈 생성 바로가기'):
                 st.switch_page("quiz_creation_page")
+            st.title("여기는 로그인한 가입자 전용 서비스입니다.")
+            if st.button('로그아웃'):
+                st.session_state.user = None
+                st.experimental_rerun()
+            elif st.button("퀴즈 저장"):
+                st.write("저장되셨습니다: 결과")
+                if st.button('퀴즈 생성 바로가기'):
+                    st.switch_page("quiz_creation_page")
+    else:
+        with placeholder.container():
+            st.title("비회원으로 퀴즈 이용하러 돌아가기")
+            if st.button('퀴즈 생성 바로가기'):
+                st.switch_page("quiz_creation_page")
+            
+            # AWS Cognito 설정
+            region_name = 'us-east-1'
+            client_id = '57gm5vjnk9p3ehk9hn5s97ropu'
+            user_pool_id = 'us-east-1_TXA2Lha1Y'
+
+            # Streamlit UI
+            st.header("로그인")
+            username = st.text_input("Username")
+            password = st.text_input("Password", type="password")
+
+            if st.button("Login"):
+                cognito_client = boto3.client('cognito-idp', region_name=region_name)
+                try:
+                    response = cognito_client.initiate_auth(
+                        ClientId=client_id,
+                        AuthFlow='USER_PASSWORD_AUTH',
+                        AuthParameters={
+                            'USERNAME': username,
+                            'PASSWORD': password
+                        }
+                    )
+                    user = response['AuthenticationResult']
+                    st.write(f"Welcome, {username}")
+                    st.session_state.user = username  # 유저네임 저장
+                    st.experimental_rerun()
+                except cognito_client.exceptions.NotAuthorizedException:
+                    st.error("인증 실패: 사용자 이름 또는 비밀번호가 올바르지 않습니다.")
+                except Exception as e:
+                    st.error(f"오류 발생: {str(e)}")
+
+            if st.button('회원가입'):
+                st.switch_page("sign")
 
 if __name__ == "__main__":
     start()
