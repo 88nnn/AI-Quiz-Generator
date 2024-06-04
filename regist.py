b9408e3e88c0670b0e9ba3baf0a4ae96f8df0d6e
88nnn <130357067+88nnn@users.noreply.github.com>
2024-06-04 11:58:37 +0900

    Update regist.py

--- regist.py
+++ regist.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 from streamlit_cognito_auth import CognitoAuthenticator
 
-pool_id = os.environ["POOL_ID"]
+pool_id = os.environ["us-east-1_TXA2Lha1Y"]
 app_client_id = os.environ["APP_CLIENT_ID"]
 app_client_secret = os.environ["APP_CLIENT_SECRET"]
 
@@ -17,6 +17,15 @@ authenticator = CognitoAuthenticator(
 is_logged_in = authenticator.login()
 if not is_logged_in:
     st.stop()
+    
+sign_in_to_homepage = sidebar.markdown('''html_button_login = (
+    html_css_login
+    + f"<a href='{login_link}' class='button-login' target='_blank'>Log In</a>"
+)
+html_button_logout = (
+    html_css_login
+    + f"<a href='{logout_link}' class='button-login' target='_blank'>Log Out</a>"
+)''', unsafe_allow_html=True)
 
 
 def logout():
