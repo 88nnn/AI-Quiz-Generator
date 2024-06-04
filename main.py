7755bafea9e91e0b58694e178addaf9efbb8d8a4
88nnn <130357067+88nnn@users.noreply.github.com>
2024-06-04 13:22:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,18 +15,13 @@ def main():
                                                "로그인", "회원가입", "퀴즈 저장"])
 
     if selected_page == "퀴즈 생성":
-        quiz_creation_page.quiz_creation_page()
+        pages.quiz_creation_page.quiz_creation_page()
     elif selected_page == "퀴즈 풀기":
-        quiz_solve_page.quiz_solve_page()
+        pages.quiz_solve_page.quiz_solve_page()
     elif selected_page == "퀴즈 리뷰":
-        quiz_grading_page.quiz_grading_page()
+        pages.quiz_grading_page.quiz_grading_page()
     elif selected_page == "로그인":
-        awscog.start()
-    #elif selected_page == "회원가입":
-     #   awscog.sign()  # 회원가입 함수 호출
-    #elif selected_page == "로그인":
-     #   awscog.login()  # 로그인 함수 호출
-
+        pages.awscog.start()
 
 if __name__ == "__main__":
     main()
