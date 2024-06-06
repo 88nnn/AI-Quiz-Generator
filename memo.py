278e3c04ac46d818c23bbbc8a3e3b36364e078fb
88nnn <130357067+88nnn@users.noreply.github.com>
2024-06-06 11:56:38 +0900

    Update memo.py

--- memo.py
+++ memo.py
@@ -1,32 +1,46 @@
-if('user_name' not in st.session_state):
-    st.session_state['user_name'] = 'unknown'
+import streamlit as st
+
+st.set_page_config(
+    page_title="memo",
+)
+
+# 세션에서 닉네임 가져오기
+if 'user' not in st.session_state:
+    st.session_state.user = None
+
+def get_username():
+    if st.session_state.user is None:
+        st.session_state.user = st.text_input("닉네임을 입력하세요", "unknown")
+    return st.session_state.user
+
+def save_memo(user, memo_text):
+    with open("memoRecord.txt", "a") as file:
+        file.write(f"{user} : {memo_text}\n")
+
+def display_memos():
+    con = st.container()
+    con.caption("Result")
+    try:
+        with open("memoRecord.txt", "r") as file:
+            for line in file:
+                con.write(line)
+    except FileNotFoundError:
+        con.write("메모가 없습니다.")
+
+def memo():
+    user = get_username()
+    st.title(f"닉네임: {user}")
     
-name = st.session_state['user_name']
+    st.sidebar.header("메모 남기기")
+    memo_text = st.text_input(label="메모 남기기", value="메모 입력")
 
-st.title("닉네임: " + name)
+    if user:
+        display_memos()
 
-#sideBar에 설정해 주어야 이동가능
-st.sidebar.header("메모 남기기")
+        if st.button("메모 등록"):
+            if memo_text:
+                save_memo(user, memo_text)
+                st.experimental_rerun()
+    else:
+        st.warning("닉네임을 입력해주세요.")
 
-memo = st.text_input(label="메모 남기기", value="메모 입력")
-
-con = st.container()
-con.caption("Result")
-
-# 기존의 메모 데이터 읽어와서 출력
-file = open("memoRecord.txt", "r")
-while True:
-    line = file.readline()
-    if not line:
-        break
-    con.write(line)
-    
-file.close()
-
-#메모 기록하기
-if st.button("메모 등록"):
-    con.write(f"{name} : {str(memo)}")
-    
-    file = open("memoRecord.txt", "a")
-    file.write(f"{name} : {str(memo)}\n")
-    file.close()
