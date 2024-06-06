78c6850773f47b5f1c4b95fb7d3415c401269e02
88nnn <130357067+88nnn@users.noreply.github.com>
2024-06-06 12:00:52 +0900

    Update memo.py

--- memo.py
+++ memo.py
@@ -8,11 +8,6 @@ st.set_page_config(
 if 'user' not in st.session_state:
     st.session_state.user = None
 
-def get_username():
-    if st.session_state.user is None:
-        st.session_state.user = st.text_input("닉네임을 입력하세요", "unknown")
-    return st.session_state.user
-
 def save_memo(user, memo_text):
     with open("memoRecord.txt", "a") as file:
         file.write(f"{user} : {memo_text}\n")
@@ -25,22 +20,23 @@ def display_memos():
             for line in file:
                 con.write(line)
     except FileNotFoundError:
-        con.write("메모가 없습니다.")
+        con.write("오답노트가 없습니다.")
 
 def memo():
     user = get_username()
-    st.title(f"닉네임: {user}")
-    
-    st.sidebar.header("메모 남기기")
-    memo_text = st.text_input(label="메모 남기기", value="메모 입력")
+    if user == "unknown" or user == "" or user == None : 
+        st.stop()
+    else:
+        st.title(f"닉네임: {user}")
 
-    if user:
+        st.sidebar.header("오답노트 남기기")
+        memo_text = st.text_input(label="오답노트 남기기", value="오답노트 입력")
+
         display_memos()
 
         if st.button("메모 등록"):
             if memo_text:
                 save_memo(user, memo_text)
                 st.experimental_rerun()
-    else:
-        st.warning("닉네임을 입력해주세요.")
 
+memo()
